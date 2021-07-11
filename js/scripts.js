fdb729ff92f5a021294ee20956534895a8b8c51b
Brian <bweloba@gmail.com>
2021-07-12 00:33:12 +0300

    add link to pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,24 @@
 
 var nav = false;
+var proj = false;
+
+function openProj() {
+    // document.getElementById("proj").style.display="block";
+    document.getElementById("proj").style.height = "180px"
+    // document.getElementById("proj").style.animationPlayState="running";
+    proj = true;
+}
+
+function closeProj() {
+    // document.getElementById("proj").style.display = "none";
+    document.getElementById("proj").style.height = "0px"
+    // document.getElementById("proj").style.animationPlayState = "paused";
+    proj = false;
+}
+
+function toggleProj() {
+    proj ? closeProj() : openProj();
+}
 
 /* Set the width of the side navigation to 250px */
 function openNav() {
@@ -15,9 +34,9 @@ function openNav() {
     document.getElementById("item4").style.opacity = "1";
     document.getElementById("item5").style.padding = "20px 8px 20px 60px";
     document.getElementById("item5").style.opacity = "1";
-    document.getElementById("sidenavDiv").style.height="500px"
-    document.getElementById("nm1").style.opacity="1";
-    document.getElementById("nm2").style.opacity="0";
+    document.getElementById("sidenavDiv").style.height = "500px"
+    document.getElementById("nm1").style.opacity = "1";
+    document.getElementById("nm2").style.opacity = "0";
     document.getElementById("nm2").style.transitionDelay = "0.9s";
     document.getElementById("nm1").style.transitionDelay = "0.9s";
     // document.getElementById("nm2").innerHTML = "Brian N Weloba";
@@ -45,6 +64,7 @@ function closeNav() {
     document.getElementById("nm2").style.transition = "0.01s";
     // document.getElementById("nm2").style.opacity = "0";
     nav = false;
+    closeProj();
 }
 
 function toggleNav() {
